ab2867e562afea2797b3d9d81028e424497e715f
willpill <65963376+willpill@users.noreply.github.com>
2024-05-14 20:01:47 -0400

    Add entrant view

--- TicketCatcher/TicketCatcherApp.swift
+++ TicketCatcher/TicketCatcherApp.swift
@@ -9,9 +9,57 @@ import SwiftUI
 
 @main
 struct TicketCatcherApp: App {
+    @State private var entered = false
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            if !entered {
+                EntrantView(entered: $entered)
+            } else {
+                ContentView()
+            }
         }
     }
+}
+
+struct EntrantView: View {
+    @Binding var entered: Bool
+    var body: some View {
+        VStack {
+            Spacer()
+            Image("Logo")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 200, height: 200)
+                .clipShape(RoundedRectangle(cornerSize: CGSize(width: 50, height: 50)))
+            Image("Title")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 200, height: 50)
+            Spacer()
+            VStack {
+                Button {
+                    entered.toggle()
+                } label: {
+                    Text("Junior Prom 2024")
+                        .frame(maxWidth: .infinity, alignment: .center)
+                }
+                .cornerRadius(20)
+                Button {
+                    entered.toggle()
+                } label: {
+                    Text("Other Events Coming Soon")
+                        .frame(maxWidth: .infinity, alignment: .center)
+                }
+                .disabled(true)
+                .cornerRadius(20)
+            }
+            .buttonStyle(.borderedProminent)
+            .padding()
+            .controlSize(.large)
+        }
+    }
+}
+
+#Preview {
+    EntrantView(entered: .constant(false))
 }
